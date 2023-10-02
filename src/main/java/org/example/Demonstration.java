fb4882b8497440d9bdc9d206e21c562794cdee2b
Miles Meloni <mrm039@bucknell.edu>
2023-10-02 14:27:07 -0400

    Started Demonstration

---
+++ src/main/java/org/example/Demonstration.java
@@ -0,0 +1,25 @@
+/* *****************************************
+ * CSCI 205 - Software Engineering and Design
+ * Fall 2023
+ * Instructor: Prof. Brian King / Prof. Joshua Stough
+ *
+ * Name: MILES MELONI
+ * Section: 9:00
+ * Date: 10/2/23
+ * Time: 2:26 PM
+ *
+ * Project: Demonstration
+ * Package: org.example
+ * Class: Demonstration
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package org.example;
+
+public class Demonstration {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
